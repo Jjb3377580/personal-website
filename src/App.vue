5f8a54a000cd8830b2425a9f2f115194d2ec2053
Jjb3377580 <1103377580@qq.com>
2022-08-07 18:47:01 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const activeIndex = ref('home')
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath)
+}
+
+</script>
+
+<template>
+
+  <el-menu :default-active="activeIndex" class="menu" mode="horizontal" :ellipsis="false" @select="handleSelect"
+    :router="true">
+    <el-menu-item index="0">LOGO</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-menu-item index="blogs">博客</el-menu-item>
+    <el-menu-item index="3">动态</el-menu-item>
+    <el-sub-menu index="4">
+      <template #title>软件</template>
+      <el-menu-item index="videoPlayer">视频播放器</el-menu-item>
+      <el-menu-item index="2-2">音频播放器</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+
+    </el-sub-menu>
+  </el-menu>
+
+  <div class="main-div">
+    <RouterView />
+  </div>
+
+</template>
+
+<style scoped>
+.menu {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.main-div {
+  padding: 0 2rem;
+}
+</style>
