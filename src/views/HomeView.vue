38f447f0076cbb49e57bc20ca00f8d20f4cdf0d8
liaozhaobo <407844384@qq.com>
2022-08-08 16:50:03 +0800

    修复首页样式bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,26 @@
-<script setup>
-</script>
-
 <template>
   home
 </template>
+
+<script>
+export default {
+    data(){
+      return {
+        conut:1
+      }
+    },
+    methods:{
+        didi(){
+          console.log(this.conut);
+        }
+    },
+    beforeCreate(){
+    },
+    created(){
+      this.didi();
+    },
+    unmounted(){
+      console.log("aaa")
+    }
+}
+</script>
