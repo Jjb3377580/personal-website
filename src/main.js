327805570f007b83f97c82883d4eb469504ba017
liaozhaobo <407844384@qq.com>
2022-08-10 16:44:09 +0800

    解决跨域问题

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+
+// Vue.config.productionTip = false
+// Vue.prototype.$axios = axios
+// axios.defaults.baseURL = '/api'        //关键代码
+// Vue.config.productionTip = false
 
 import App from './App.vue'
 import router from './router'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,24 +3,35 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {request} from '../router/request.js'
+// var data=axios(
+//     {
+//       url:"/api/tools/zimi",
+//       type:"get",
+//       data:{
+//         "question":"***"
+//       },
+//       contentType:"text",
+//       processData: false,
+//       dataType:"text"
+//     }
+// ).then(function (response){
+  // console.log(response);})
 export default {
     data(){
       return {
         conut:1
       }
     },
-    methods:{
-        didi(){
-          console.log(this.conut);
-        }
-    },
-    beforeCreate(){
-    },
-    created(){
-      this.didi();
-    },
-    unmounted(){
-      console.log("aaa")
-    }
+  created(){
+       request({
+        url:'/'
+      }).then(res=>{
+        console.log(res);
+      },err=>{
+        console.log(err);
+      })
+  }
 }
 </script>
